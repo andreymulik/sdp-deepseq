6c4fb1ffb88094b84fd149da95bb1917735a8575
Andrey Mulik <work.a.mulik@gmail.com>
2019-11-07 13:46:09 +0300

    new version 0.2

---
+++ README.md
@@ -0,0 +1,13 @@
+# SDP DeepSeq
+
+This is an extension, that provides NFData instances for SDP structures.
+
+## Reasons
+
+Package deepseq requires array package so i can't use it.
+
+## Versioning
+
+sdp4bytestring follows of the[Haskell PVP](https://pvp.haskell.org) and SDP
+wrapper rules.
+
---
+++ src/Control/DeepSeq/SDP.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE Trustworthy, MagicHash #-}
+
+{- |
+    Module      :  Control.DeepSeq.SDP
+    Copyright   :  (c) Andrey Mulik 2019
+    License     :  BSD-style
+    Maintainer  :  work.a.mulik@gmail.com
+    Portability :  portable
+    
+    @Control.DeepSeq.SDP@ contains 'NFData' instances for SDP types.
+-}
+module Control.DeepSeq.SDP
+(
+  -- * Exports.
+  module Control.DeepSeq
+)
+where
+
+import Prelude ()
+import SDP.SafePrelude
+
+import Control.DeepSeq
+
+import SDP.Array
+import SDP.Bytes
+import SDP.Unrolled
+import SDP.ByteList
+
+default ()
+
+--------------------------------------------------------------------------------
+
+{- NFData instances. -}
+
+instance (NFData e) => NFData (Unlist  e)
+instance (NFData e) => NFData (Ublist  e)
+instance (NFData e) => NFData (SArray# e) where rnf = rnf . listL
+instance (NFData e) => NFData (SBytes# e) where rnf = rwhnf
+
+instance (NFData i, NFData e) => NFData (Array i e)
+instance (NFData i, NFData e) => NFData (Bytes i e)
+instance (NFData i, NFData e) => NFData (Unrolled i e)
+instance (NFData i, NFData e) => NFData (ByteList i e)
+
+
